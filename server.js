97df5e52aff1b7e485d600af70877651500edad6
comi.hu <senline39518@gmail.com>
2017-12-14 14:27:55 +0800

    auto deploy from github test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World! From AWS CodeDeploy!'))
 
 var server = app.listen(process.env.PORT || 3000, () => console.log(`Example app listening on port ${server.address().port}`))
